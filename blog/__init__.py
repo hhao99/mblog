abd78a728d050ecc93382f6670adf41cfdde6fe3
hhao99 <hhao99@163.com>
2020-01-30 00:03:05 +0800

    add bootstrap support

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,8 +1,10 @@
 import os
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
 def create_app():
     app = Flask(__name__)
+    Bootstrap(app)
 
     app.config.from_mapping(
         SECRECT_KEY='dev',
